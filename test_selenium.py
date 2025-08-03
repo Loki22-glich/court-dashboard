fdc184773dc761b5919629828b71802bd06faa6d
banothlokesh <lukkybanoth@gmail.com>
2025-08-03 18:54:18 +0530

    Update test_selenium.py

--- test_selenium.py
+++ test_selenium.py
@@ -1,36 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.service import Service
 import time
 
-# Optional: If you know where your geckodriver is, set it here:
-# service = Service("/opt/homebrew/bin/geckodriver")
-
-# Firefox options
 options = Options()
-# Comment out headless if you want to see the browser
-# options.add_argument("--headless")
-
-# Start Firefox browser
-# If you installed geckodriver with brew, you likely don't need to specify service
 driver = webdriver.Firefox(options=options)
 
-# Load Delhi High Court search page
 driver.get("https://delhihighcourt.nic.in/case.asp")
 
 # Fill the form
 driver.find_element("name", "ctype").send_keys("W.P. (C)")
-driver.find_element("name", "cno").send_keys("12345")
-driver.find_element("name", "cyear").send_keys("2023")
+driver.find_element("name", "cno").send_keys("6094")
+driver.find_element("name", "cyear").send_keys("2022")
 
-# Click the submit button
-driver.find_element("xpath", "//input[@type='submit']").click()
+# Submit form using JavaScript instead of .click()
+driver.execute_script("document.forms[0].submit()")
 
-# Wait and print results
+# Wait and show output
 time.sleep(5)
-print("Page title:", driver.title)
+print("Title:", driver.title)
 print("URL:", driver.current_url)
 
-# Keep the browser open to see output
 time.sleep(30)
 driver.quit()
